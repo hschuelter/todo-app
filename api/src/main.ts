e52f4365362dd2009d5bb52694fd200700d6ea9e
Arthur <arthur.schuelter@gmail.com>
2025-07-12 11:16:07 -0300

    ✨ feat (api): added swagger documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ToDo API')
+    .setDescription('Manage your tasks with this ToDo API')
+    .setVersion('0.2.1')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -14,6 +24,6 @@ async function bootstrap() {
   app.enableCors();
   
   await app.listen(3000);
-  console.log('Todo API is running on http://localhost:3000');
+  console.log('Documentation on http://localhost:3000/docs');
 }
 bootstrap();
